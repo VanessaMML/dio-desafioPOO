853f2cd0bf7f606e6712910a5c61eeed85296445
VanessaMML <nessam348@gmail.com>
2024-06-07 18:43:44 -0300

    First Commit

---
+++ desafio-dioPOO/src/Main.java
@@ -0,0 +1,33 @@
+import java.time.LocalDate;
+
+import br.com.dio.desafio.dominio.Curso;
+import br.com.dio.desafio.dominio.Mentoria;
+
+public class Main {
+	public static void main(String[] args) {
+		
+		//Objetos Cursos
+		Curso curso1 = new Curso();
+		curso1.setTitulo("Curso Java");
+		curso1.setDescricao("Descrição curso Java");
+		curso1.setCargaHoraria(8);
+		
+		Curso curso2 = new Curso();
+		curso2.setTitulo("Curso JS");
+		curso2.setDescricao("Descrição curso JS");
+		curso2.setCargaHoraria(10);
+		
+		System.out.println(curso1);
+		System.out.println(curso2);
+		
+		//Objetos Mentoria
+		Mentoria mentoria = new Mentoria();
+		mentoria.setTitulo("Mentoria Java");
+		mentoria.setDescricao("Descrição mentoria Java");
+		mentoria.setData(LocalDate.now());
+		
+		System.out.println(mentoria);
+		
+	}
+
+}
